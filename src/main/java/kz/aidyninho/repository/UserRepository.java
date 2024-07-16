30d9d80835d415d372c27abdf1c92d8c2b4aacc7
aidyninho <ryzyk.aidyn@gmail.com>
2024-07-17 02:00:26 +0500

    T-2: Add/update Ticket, User repositories

--- src/main/java/kz/aidyninho/repository/UserRepository.java
+++ src/main/java/kz/aidyninho/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package kz.aidyninho.repository;
 
-import kz.aidyninho.model.Ticket;
+import kz.aidyninho.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
